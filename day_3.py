b6b046d287d03785df1970ccda4beb41d97da2ce
Alienmenno <alienmenno@hotmail.com>
2019-12-08 23:57:26 +0100

    Day 3 - Completed (clean up required)

--- day_3.py
+++ day_3.py
@@ -179,6 +179,9 @@ def find_intersections(node_seq_a, node_seq_b):
 
     return intersections
 
+def find_minimal_tuple_sum(tuple_seq):
+    return min([abs(tpl[0]) + abs(tpl[1]) for tpl in tuple_seq])
+
 def find_manhattan_distance(seq_a, seq_b):
     """
     >>> find_manhattan_distance( \
@@ -198,16 +201,117 @@ def find_manhattan_distance(seq_a, seq_b):
 
     intersections = find_intersections(node_seq_a, node_seq_b)
 
-    nearest_node = min([abs(intersect[0]) + abs(intersect[1]) for intersect in intersections])
+    nearest_node = find_minimal_tuple_sum(intersections)
 
     return nearest_node
 
 def solve_fuel_wirring():
+    """
+    >>> solve_fuel_wirring()
+    207
+    """
     codes = read_codes('day_3_input.txt')
     return find_manhattan_distance(codes[0], codes[1])
 
+"""
+--- Part Two ---
+
+It turns out that this circuit is very timing-sensitive; you actually need to minimize the signal delay.
+
+To do this, calculate the number of steps each wire takes to reach each intersection; choose the intersection where the sum of both wires' steps is lowest. If a wire visits a position on the grid multiple times, use the steps value from the first time it visits that position when calculating the total value of a specific intersection.
+
+The number of steps a wire takes is the total number of grid squares the wire has entered to get to that location, including the intersection being considered. Again consider the example from above:
+
+...........
+.+-----+...
+.|.....|...
+.|..+--X-+.
+.|..|..|.|.
+.|.-X--+.|.
+.|..|....|.
+.|.......|.
+.o-------+.
+...........
+
+In the above example, the intersection closest to the central port is reached after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps by the second wire for a total of 20+20 = 40 steps.
+
+However, the top-right intersection is better: the first wire takes only 8+5+2 = 15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30 steps.
+
+Here are the best steps for the extra examples from above:
+
+    R75,D30,R83,U83,L12,D49,R71,U7,L72
+    U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+    R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+    U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+
+What is the fewest combined steps the wires must take to reach an intersection?
+"""
+
+def compute_cost(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def find_intersection_costs(node_seq_a, node_seq_b):
+    prev_node_a = node_seq_a[0]
+    prev_node_b = node_seq_b[0]
+    cost_a = 0
+    cost_b = 0
+
+    intersection_costs = []
+
+    for node_a in node_seq_a[1:]:
+        for node_b in node_seq_b[1:]:
+            intersection = linear_interp_intersect([prev_node_a, node_a], [prev_node_b, node_b])
+
+            if intersection:
+                intersection_costs.append((compute_cost(prev_node_a, intersection) + cost_a, \
+                                          compute_cost(prev_node_b, intersection) + cost_b))
+
+            # Check reverse
+            intersection = linear_interp_intersect([prev_node_b, node_b], [prev_node_a, node_a])
+
+            if intersection:
+                intersection_costs.append((compute_cost(prev_node_a, intersection) + cost_a, \
+                                          compute_cost(prev_node_b, intersection) + cost_b))
+
+            cost_b += compute_cost(prev_node_b, node_b)
+            prev_node_b = node_b
+        cost_a += compute_cost(prev_node_a, node_a)
+        prev_node_a = node_a
+        prev_node_b = node_seq_b[0]
+        cost_b = 0
+
+    return intersection_costs
+
+def find_closest_by_wire(seq_a, seq_b):
+    """
+    >>> find_closest_by_wire( \
+        ['R8','U5','L5','D3'],['U7','R6','D4','L4'])
+    30
+    >>> find_closest_by_wire( \
+        ['R75','D30','R83','U83','L12','D49','R71','U7','L72'], \
+        ['U62','R66','U55','R34','D71','R55','D58','R83'])
+    610
+    >>> find_closest_by_wire( \
+        ['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51'], \
+        ['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7'])
+    410
+    """
+    node_seq_a = parse_input_sequence(seq_a)
+    node_seq_b = parse_input_sequence(seq_b)
+
+    intersection_costs = find_intersection_costs(node_seq_a, node_seq_b)
+
+    return find_minimal_tuple_sum(intersection_costs)
+
+def find_closest_fuel_wire():
+    """
+    >>> find_closest_fuel_wire()
+    21196
+    """
+    codes = read_codes('day_3_input.txt')
+    return find_closest_by_wire(codes[0], codes[1])
+
 #------------------------------------------------------------------------------#
 if __name__ == "__main__":
-    # import doctest
-    # doctest.testmod()
-    print(solve_fuel_wirring())
+    import doctest
+    doctest.testmod()
